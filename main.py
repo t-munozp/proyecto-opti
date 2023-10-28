dbb465bd96bd3593badb76b7f6a66b9cbc3da733
Gustavo Vidal <govidal@uc.cl>
2023-10-27 21:39:01 -0300

    awawa

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from gurobipy import GRB, Model, quicksum
+from process_data import *
 
 m = Model()
 m.setParam("TimeLimit", 60)
@@ -16,40 +17,33 @@ cantidadmaxdevehiculos = 7
 
 # VEHÍCULOS
 # Emisión co2 vehículo v
-rho = {1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7}
+rho, epsilon, M, B, D, Y, Z = vehiculos()
 
-# Eficiencia vehículo v
-epsilon = {1: 1, 2: 1, 3: 2, 4: 0.3, 5: 0.4, 6: 0.2, 7: 0.2}
 
 # Carga máxima (en kg) vehículo v
-M = {1: 300, 2: 300, 3: 300, 4: 550, 5: 500, 6: 1000, 7: 1000}
+
 
 # Peso (en kg) del vehículo v
 W = {1: 1000, 2: 1000, 3: 1000, 4: 1750, 5: 1500, 6: 3000, 7: 3000}
 
 # Precio que cobra un conductor por conducir un camión 8 horas
-omega = 29536
+omega = 295368
 
 # Precio que cobra un conductor por conducir un bus por 8 horas
 theta = 27080
 
-# Cantidad de jornadas laborales necesarias para recorrer el trayecto t en camion.
-gamma = {}
-
 # Cantidad de jornadas laborales necesarias para recorrer el trayecto t en bus.
-beta = {}
+# Cantidad de jornadas laborales necesarias para recorrer el trayecto t en camion.
+beta, gamma = jornadas()
 
 # Vehículo v bencinero {0,1}
-B = {1: 0, 2: 0, 3: 1, 4: 0, 5: 1, 6: 1, 7: 0}
 
-# Vehículo v petrolero {0,1}
-D = {1: 1, 2: 1, 3: 0, 4: 1, 5: 0, 6: 0, 7: 1}
 
 # Costo de bencina por litro
-S = 50
+S = 1356
 
 # Costo de petróleo por litro
-R = 30
+R = 1200
 
 
 # CARGA
@@ -60,15 +54,15 @@ R = 30
 # H = 4
 
 # Peso elemento c
-o = {1: 50, 2: 200, 3: 30, 4: 1000, 5: 800, 6: 10}
+o = elementos()
 
 # Peso persona h
-p = {1: 50, 2: 60, 3: 70, 4: 80}
+p = personas()
 
 
 # OTROS
 # Kilómetros en trayecto t
-k = {1: 100, 2: 200, 3: 300, 4: 350, 5: 400, 6: 140, 7: 240, 8: 320, 9: 500, 10: 800}
+k = distancias()
 
 # Presupuesto transporte
 tau = 50000000
@@ -89,37 +83,49 @@ m.update()
 
 # RESTRICCIONES
 # El peso de la carga del vehículo v no debe superar el máximo permitido
-m.addConstrs((quicksum(g[h,v,t] * p[h] for h in H) + quicksum(i[v,c,t] * o[c] for c in C) <= M[v] for v in V for t in T), name="R1")
+m.addConstrs((quicksum(g[h, v, t] * p[h] for h in H) + quicksum(i[v, c, t] * o[c]
+             for c in C) <= M[v] for v in V for t in T), name="R1")
 
 # La cantidad de vehículos v usados en el trayecto t no debe superar la cantidad disponible de vehículos
-m.addConstrs((quicksum(x[v,t] for v in V) <= cantidadmaxdevehiculos for t in T), name="R2") #cantidadmaxdevehiculos HAY QUE DEF
+# cantidadmaxdevehiculos HAY QUE DEF
+m.addConstrs((quicksum(x[v, t] for v in V) <=
+             cantidadmaxdevehiculos for t in T), name="R2")
 
 # El peso mínimo de la carga del vehículo v tiene que ser mayor o igual al 50% de la carga máxima de este
-m.addConstrs((0.5 * M[v] <= quicksum(g[h,v,t] * p[h] for h in H) + quicksum(i[v,c,t] * o[c] for c in C) for v in V for t in T), name="R3")
+m.addConstrs((0.5 * M[v] <= quicksum(g[h, v, t] * p[h] for h in H) +
+             quicksum(i[v, c, t] * o[c] for c in C) for v in V for t in T), name="R3")
 
 # Los costos de transporte del tour no deben superar el presupuesto para transporte
-m.addConstr((quicksum(quicksum(x[v,t]*k[t] for t in T) * (1/epsilon[v]) * (S * B[v] + R * D[v]) for v in V) <= tau), name="R4")
+m.addConstr((quicksum(quicksum(x[v, t]*k[t] for t in T) * (1/epsilon[v])
+            * (S * B[v] + R * D[v]) for v in V) <= tau), name="R4")
 
 # Los costos de sueldos no deben superar el presupuesto de salario
-m.addConstr((quicksum(quicksum(x[v,t]*omega[v] for t in T) for v in V) <= Q), name="R5")
+m.addConstr((quicksum(quicksum(x[v, t]*omega[v]
+            for t in T) for v in V) <= Q), name="R5")
 
 # Los gastos totales deben ser menores o igules al presupuesto final
-m.addConstr((quicksum(quicksum(x[v,t]*omega[v] for t in T)for v in V) + quicksum(quicksum(x[v,t]*k[t] for t in T) * (1/epsilon[v]) * (S * B[v] + R * D[v]) for v in V) <= U), name="R6")
+m.addConstr((quicksum(quicksum(x[v, t]*omega[v] for t in T)for v in V) + quicksum(quicksum(
+    x[v, t]*k[t] for t in T) * (1/epsilon[v]) * (S * B[v] + R * D[v]) for v in V) <= U), name="R6")
 
-# En cada trayecto se deben transportar todos los elementos 
-m.addConstrs((quicksum(quicksum(i[v,c,t] for c in C) for v in V) <= len(C) for t in T), name="R7")
-m.addConstrs((quicksum(quicksum(i[v,c,t] for c in C) for v in V) >= len(C) for t in T), name="R")
- 
+# En cada trayecto se deben transportar todos los elementos
+m.addConstrs((quicksum(quicksum(i[v, c, t] for c in C)
+             for v in V) <= len(C) for t in T), name="R7")
+m.addConstrs((quicksum(quicksum(i[v, c, t] for c in C)
+             for v in V) >= len(C) for t in T), name="R")
+
 # En cada trayecto se deben transportar todas las personas
-m.addConstrs((quicksum(quicksum(g[h,v,t] for h in H) for v in V) <= len(H) for t in T), name="R9")
-m.addConstrs((quicksum(quicksum(g[h,v,t] for h in H) for v in V) >= len(H) for t in T), name="R10")
+m.addConstrs((quicksum(quicksum(g[h, v, t] for h in H)
+             for v in V) <= len(H) for t in T), name="R9")
+m.addConstrs((quicksum(quicksum(g[h, v, t] for h in H)
+             for v in V) >= len(H) for t in T), name="R10")
 
 
 m.update()
 
 # FUNCIÓN OBJETIVO
 
-f_objetivo = (quicksum(quicksum(x[v, t] * k[t] * (quicksum(i[v, c, t] * o[c] for c in C)) + W[v] for t in T) * rho[v] for v in V))
+f_objetivo = (quicksum(quicksum(x[v, t] * k[t] * (quicksum(i[v, c, t] * o[c]
+              for c in C)) + W[v] for t in T) * rho[v] for v in V))
 m.setObjective(f_objetivo, GRB.MINIMIZE)
 
 m.optimize()
--- process_data.py
+++ process_data.py
@@ -1,148 +1,119 @@
 import csv
-
-M = range(1 , 5 + 1) #materiales
-I = range(1, 7 + 1) #regiones
-D = range(1, 3 + 1) #dimensión
-T = range(1, 50 + 1) #tiempo
-
-materiales = {'Rachel 35%': 1, 'Rachel 50%': 2, 'Tela quirurguica': 3, 'Costal de fique': 4, 'Guata': 5}
-regiones = {'Atacama': 1, 'Coquimbo': 2, 'Valparaiso': 3, 'Metropolitana': 4, 'Ohiggins': 5, 'Maule': 6, 'Nuble': 7}
-dimensiones = {'48': 1, '100': 2, '150': 3}
+from os import path
 
 
-def demanda_anual():
+def distancias():
+    ruta_distancias = path.join("datos", "Distancias.csv")
     diccionario = {}
-    with open('data/demanda3.csv', 'r') as archivo:
+    with open(ruta_distancias, 'r') as archivo:
         data = csv.reader(archivo)
-        next(data)
-        for fila in data:
-            region, año, demanda = fila
-            numero_region_dict = regiones[region]
-            diccionario[numero_region_dict, int(año) - 2024] = float(demanda)*(10**6) * 0.03
-    return diccionario
-
-def demanta_total():
-    demanta_total = 0
-    with open('data/demanda3.csv', 'r') as archivo:
-        data = csv.reader(archivo)
-        next(data)
-        for fila in data:
-            region, año, demanda = fila
-            demanta_total += float(demanda)*(10**6) * 0.03
-    return demanta_total
-
-def costo_mantencion():
-
-    diccionario = {}
-
-    with open('data/costo_mantencion.csv', 'r') as archivo:
-        data = csv.reader(archivo)
-        next(data)
-        for fila in data:
-            material, dimension, precio = fila
-            numero_material = materiales[material]
-            numero_dimension = dimensiones[dimension]
-            for año in range(1,51):
-                for region in range(1,7 + 1):
-                    diccionario[numero_material, region, numero_dimension, año] = float(precio)
-
+        fila = 0
+        for filas in data:
+            if fila == 0:
+                fila += 1
+            else:
+                trayecto, distancia = filas
+                diccionario[int(trayecto)] = int(distancia)
     return diccionario
 
 
-def costo_instalacion():
-
+def personas():
+    ruta_distancias = path.join("datos", "Personas.csv")
     diccionario = {}
-
-    with open('data/costo_instalacion.csv', 'r') as archivo:
+    with open(ruta_distancias, 'r') as archivo:
         data = csv.reader(archivo)
-        next(data)
-        for fila in data:
-            material, dimension, precio = fila
-            numero_material = materiales[material]
-            numero_dimension = dimensiones[dimension]
-            for año in range(1,51):
-                for region in range(1,7 + 1):
-                    diccionario[numero_material, region, numero_dimension, año] = float(precio)
-        
-    
+        fila = 0
+        for n, filas in enumerate(data):
+            if fila == 0:
+                fila += 1
+            else:
+                diccionario[n] = 80.7
     return diccionario
 
 
-
-#no debería depender de la región, es el mismo atrapanieblas
-
-def capacidad_maxima():
-    diccionario = {}
-    with open('data/capacidad_maxima.csv', 'r') as archivo:
+def jornadas():
+    ruta_distancias = path.join("datos", "Jornadas.csv")
+    buses = {}
+    camiones = {}
+    with open(ruta_distancias, 'r') as archivo:
         data = csv.reader(archivo)
-        next(data)
-        for fila in data:
-            material, dimension, recoleccion  = fila
-            numero_dimension = dimensiones[dimension]
-            numero_material = materiales[material]
-            for region in range(1,7 + 1):
-                diccionario[numero_material,region,numero_dimension] = float(recoleccion)*365
+        fila = 0
+        for filas in data:
+            if fila == 0:
+                fila += 1
+            else:
+                trayecto, camion, bus = filas
+                buses[int(trayecto)] = int(bus)
+                camiones[int(trayecto)] = int(camion)
+    return buses, camiones
 
 
-    return diccionario
+def elementos():
+    ruta_distancias = path.join("datos", "Elementos.csv")
+    pesos = {}
 
-
-def humedad_anual():
-    diccionario = {}
-    with open('data/humedad.csv', 'r') as archivo:
+    with open(ruta_distancias, 'r') as archivo:
         data = csv.reader(archivo)
-        next(data)
-        for fila in data:
-            region, humedad = fila
-            numero_region = regiones[region]
-            for año in range(1,51):
-                diccionario[numero_region,año] = float(humedad)
-    return diccionario
+        fila = 0
+        for n, filas in enumerate(data):
+            if fila == 0:
+                fila += 1
+            elif (filas != []):
+                pesos[n] = float(filas[1])
+    return pesos
 
-def eficiencia_material():
-    diccionario = {}
-    with open('data/eficiencia.csv', 'r') as archivo:
+
+def vehiculos():
+    ruta_distancias = path.join("datos", "Vehiculos.csv")
+    Rho = {}
+    Epsilon = {}
+    M = {}
+    B = {}
+    D = {}
+    Y = {}
+    Z = {}
+
+    with open(ruta_distancias, 'r') as archivo:
         data = csv.reader(archivo)
-        next(data)
-        for fila in data:
-            material, eficiencia = fila
-            numero_material = materiales[material]
-            diccionario[numero_material] = float(eficiencia)
-    
-    return diccionario
+        fila = 0
+        for n, filas in enumerate(data):
+            if fila == 0:
+                fila += 1
+            elif (filas != []):
+                _, _, rho, epsilon, m, b, d, y, z = filas
+                Rho[n] = int(rho)
+                Epsilon[n] = float(epsilon)
+                M[n] = int(m)
+                B[n] = int(b)
+                D[n] = int(d)
+                Y[n] = int(y)
+                Z[n] = int(z[0:-1])
+    return Rho, Epsilon, M, B, D, Y, Z
 
-def superficie_efectiva():
-    diccionario = {}
-    with open('data/superficie.csv', 'r') as archivo:
+
+def webada2():
+    ruta_distancias = path.join("datos", "Precios.csv")
+    Rho = {}
+    Epsilon = {}
+    M = {}
+    B = {}
+    D = {}
+    Y = {}
+    Z = {}
+
+    with open(ruta_distancias, 'r') as archivo:
         data = csv.reader(archivo)
-        next(data)
-        for fila in data:
-            region, superficie = fila
-            numero_region = regiones[region]
-            diccionario[numero_region] = float(superficie) * (10**6)
-    
-    return diccionario
-
-def area_ocupada():
-    diccionario = {}
-    with open('data/area.csv', 'r') as archivo:
-        data = csv.reader(archivo)
-        next(data)
-        for fila in data:
-            dimension, area = fila
-            numero_dimension = dimensiones[dimension]
-            diccionario[numero_dimension] = float(area)
-
-    return diccionario
-
-def plazo_mantenimiento():
-    diccionario = {}
-    with open('data/plazo_mantenimiento.csv', 'r') as archivo:
-        data = csv.reader(archivo)
-        next(data)
-        for fila in data:
-            region, plazo = fila
-            numero_region = regiones[region]
-            diccionario[numero_region] = int(plazo) - 2
-    
-    return diccionario
+        fila = 0
+        for n, filas in enumerate(data):
+            if fila == 0:
+                fila += 1
+            elif (filas != []):
+                _, _, rho, epsilon, m, b, d, y, z = filas
+                Rho[n] = int(rho)
+                Epsilon[n] = float(epsilon)
+                M[n] = int(m)
+                B[n] = int(b)
+                D[n] = int(d)
+                Y[n] = int(y)
+                Z[n] = int(z[0:-1])
+    return Rho, Epsilon, M, B, D, Y, Z
